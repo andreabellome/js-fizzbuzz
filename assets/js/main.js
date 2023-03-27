4ead895c1000204ba29f604c8f5b481690381f40
Andrea Bellome <andrea.bellome@gmail.com>
2023-03-27 17:59:11 +0200

    comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,16 @@ for(let i = 1; i<=100; i++){
 
     if( i % 3 == 0 && i % 5 == 0 ){
         createDiv.className = 'box box-yellow'; /* add classes to the div */
-        createDiv.innerHTML = 'FizzBuzz';
+        createDiv.innerHTML = 'FizzBuzz'; /* write in the div */
     } else if(i % 3 == 0){
         createDiv.className = 'box box-green'; /* add classes to the div */
-        createDiv.innerHTML = 'Fizz';
+        createDiv.innerHTML = 'Fizz'; /* write in the div */
     } else if(i % 5 == 0){
         createDiv.className = 'box box-blue'; /* add classes to the div */
-        createDiv.innerHTML = 'Buzz';
+        createDiv.innerHTML = 'Buzz'; /* write in the div */
     } else{
         createDiv.className = 'box'; /* add classes to the div */
-        createDiv.innerHTML = i;
+        createDiv.innerHTML = i; /* write in the div */
     }
 
     containerHTML.append(createDiv); /* add the div with such classes to the container */
